98fb2c4b312906906611b42beb73e2093823680d
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-04-25 08:30:05 +0200

    Simplify Task class implementation
    Have just 'run' as reserved keyword and check required args in
    base class.

--- se/tasks.py
+++ se/tasks.py
@@ -5,35 +5,31 @@ from se.helpers import render_string_recursive
 
 class Task(object):
 
-    def __init__(self, dict):
-        # Reserved identifiers for functions that subclasses must implement
-        _reserved_identifiers = {'run'}
-
-        # Make sure only subclasses of Task are instantiated
-        assert (self.__class__.__name__ is not 'Task'), \
-               'Do not create Tasks directly, use subclasses!'
-
-        # Make sure that the Task is created from a dictionary and that the
-        # dictionary does not contain any of the reserved identifiers as a key
+    def __init__(self, dictionary, *required_args):
         try:
-            assert (not dict.keys() & _reserved_identifiers), \
-                   'Reserved identifiers used when creating {} task: {}'.format(self.__class__.__name__,
-                                                                    dict.keys() & _reserved_identifiers)
-        except AttributeError:
-            raise TypeError('{} task must be created from dictionary'.format(self.__class__.__name__))
-
-        # Add all key/value pairs as instance members
-        self.__dict__.update(dict)
+            # Make sure that 'run' is not a dictionary key, it's reserved for the run() method!
+            if 'run' in dictionary:
+                raise RuntimeError('Reserved identifier "run" used when creating task')
+            # Add all key/value pairs as instance members
+            self.__dict__.update(dictionary)
+        except TypeError:
+            raise TypeError('Tasks must be created from a dictionary')
+        # Make sure all required arguments are present
+        for arg in required_args:
+            if arg not in self.__dict__:
+                raise UnboundLocalError(self.__class__.__name__+' is missing the "'+str(arg)+'" argument')
 
     def __repr__(self):
         return '{}({})'.format(self. __class__.__name__, self.__dict__)
+
+    def run(self, **kwargs):
+        raise NotImplementedError('Task.run() method called, which is an abstract method')
 
 
 class Echo(Task):
 
     def __init__(self, dict):
-        super(Echo, self).__init__(dict)
-        assert ('msg' in self.__dict__), 'Echo task needs a "msg" attribute'
+        super(Echo, self).__init__(dict, 'msg')
 
     def __str__(self):
         return 'Echo: {}'.format(self.msg)
@@ -48,9 +44,7 @@ class Echo(Task):
 class Copy(Task):
 
     def __init__(self, dict):
-        super(Copy, self).__init__(dict)
-        assert ('src' in self.__dict__), 'Copy task needs a "src" attribute'
-        assert ('dst' in self.__dict__), 'Copy task needs a "dst" attribute'
+        super(Copy, self).__init__(dict, 'src', 'dst')
 
     def __str__(self):
         return 'Copy: {} --> {}'.format(self.src, self.dst)
@@ -65,9 +59,7 @@ class Copy(Task):
 class Link(Task):
 
     def __init__(self, dict):
-        super(Link, self).__init__(dict)
-        assert ('src' in self.__dict__), 'Link task needs a "src" attribute'
-        assert ('dst' in self.__dict__), 'Link task needs a "dst" attribute'
+        super(Link, self).__init__(dict, 'src', 'dst')
 
     def __str__(self):
         return 'Link: {} --> {}'.format(self.src, self.dst)
@@ -79,8 +71,7 @@ class Link(Task):
 class Command(Task):
 
     def __init__(self, dict):
-        super(Command, self).__init__(dict)
-        assert ('name' in self.__dict__), 'Command task needs a "name" attribute'
+        super(Command, self).__init__(dict, 'name')
 
         if 'args' not in self.__dict__:
             self.args = []
