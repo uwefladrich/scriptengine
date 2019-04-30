b8bbdeea8dc507ea531c430600044c88b0c4688b
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-04-30 09:54:58 +0200

    Add implementation of command task

--- se/tasks/command.py
+++ se/tasks/command.py
@@ -1,4 +1,7 @@
+import subprocess
+
 from se.tasks import Task
+from se.helpers import render_string_recursive
 
 
 class Command(Task):
@@ -6,14 +9,14 @@ class Command(Task):
     def __init__(self, dict):
         super(Command, self).__init__(dict, 'name')
 
-        if 'args' not in self.__dict__:
-            self.args = []
-        else:
-            # Assert args is a list
-            pass
-
     def __str__(self):
-        return 'Running command "{}" with argument list "{}"'.format(self.name, self.args)
+        return 'Running command "{}" with arguments "{}"'.format(self.name,
+                                                                 getattr(self, 'args', None))
 
-    def run(self):
-        print(self)
+    def run(self, *, dryrun=False, **kwargs):
+        if dryrun:
+            print(render_string_recursive(str(self), **kwargs))
+        else:
+            cmd = [self.name] \
+                + [render_string_recursive(s, **kwargs) for s in getattr(self, 'args', [])]
+            subprocess.run(cmd)
