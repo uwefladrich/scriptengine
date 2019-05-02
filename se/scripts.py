d30339171cd86910218870086b3f28044ec7b227
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-05-02 14:36:01 +0200

    Restructure create_job_from_dict for config-as-task

--- se/scripts.py
+++ se/scripts.py
@@ -1,6 +1,7 @@
 class SimpleScriptEngine(object):
 
-    def run(self, *, dryrun=False, script=[], **config):
+    def run(self, *, dryrun=False, script=[]):
+        config = {}
         for job in script:
             cfg = job.run(dryrun=dryrun, **config)
             if cfg is not None:
