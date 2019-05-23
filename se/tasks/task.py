5b1f1eb3014d841f9853a9c77cc2a73ca691d5e6
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-05-23 20:08:29 +0200

    Remove obsolete class definitions

--- se/tasks/task.py
+++ se/tasks/task.py
@@ -1,8 +1,3 @@
-import shutil
-
-from se.helpers import render_string_recursive
-
-
 class Task(object):
 
     def __init__(self, dictionary, *required_args):
@@ -24,63 +19,3 @@ class Task(object):
 
     def run(self, **config):
         raise NotImplementedError('Task.run() method called, which is an abstract base class method')
-
-
-class Echo(Task):
-
-    def __init__(self, dict):
-        super(Echo, self).__init__(dict, 'msg')
-
-    def __str__(self):
-        return 'Echo: {}'.format(self.msg)
-
-    def run(self, *, dryrun=False, **config):
-        if dryrun:
-            print(render_string_recursive(str(self), **config))
-        else:
-            print('{}'.format(render_string_recursive(self.msg, **config)))
-
-
-class Copy(Task):
-
-    def __init__(self, dict):
-        super(Copy, self).__init__(dict, 'src', 'dst')
-
-    def __str__(self):
-        return 'Copy: {} --> {}'.format(self.src, self.dst)
-
-    def run(self, *, dryrun=False, **config):
-        if dryrun:
-            print(render_string_recursive(str(self), **config))
-        else:
-            shutil.copy(render_string_recursive(self.src, **config), render_string_recursive(self.dst, **config))
-
-
-class Link(Task):
-
-    def __init__(self, dict):
-        super(Link, self).__init__(dict, 'src', 'dst')
-
-    def __str__(self):
-        return 'Link: {} --> {}'.format(self.src, self.dst)
-
-    def run(self):
-        print(self)
-
-
-class Command(Task):
-
-    def __init__(self, dict):
-        super(Command, self).__init__(dict, 'name')
-
-        if 'args' not in self.__dict__:
-            self.args = []
-        else:
-            # Assert args is a list
-            pass
-
-    def __str__(self):
-        return 'Running command "{}" with argument list "{}"'.format(self.name, self.args)
-
-    def run(self):
-        print(self)
