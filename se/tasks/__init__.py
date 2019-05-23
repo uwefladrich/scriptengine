6644fdafe9ffffb58d8e60deb020b9f962cb7e7f
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-05-23 20:13:27 +0200

    Remove Link from se.task module

--- se/tasks/__init__.py
+++ se/tasks/__init__.py
@@ -1,7 +1,7 @@
-from se.tasks.task    import Task
+from se.tasks.task import Task
 from se.tasks.configuration import Configuration
-from se.tasks.echo    import Echo
-from se.tasks.files   import Copy, Link
+from se.tasks.echo import Echo
+from se.tasks.copy import Copy
 from se.tasks.command import Command
 from se.tasks.template import Template
 from se.tasks.getenv import Getenv
