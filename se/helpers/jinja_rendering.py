82cb77abf850a53bf77c0b72e16984a0069b35d0
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-08-27 13:35:50 +0200

    Reimplement using context dict

--- se/helpers/jinja_rendering.py
+++ se/helpers/jinja_rendering.py
@@ -1,14 +1,14 @@
 """ScriptEngine helpers: Jinja2 rendering"""
 
 from distutils.util import strtobool
-from jinja2 import Template
+from jinja2 import Template, TemplateSyntaxError
 
 
-def render_string(string, recursive=True, boolean=False, **kwargs):
+def render_string(string, context, recursive=True, boolean=False):
     """ Renders a string with Jinja2.
 
     The string is rendered via jinja2.Template().render(), either once or
-    recursively until no further variables can be substituted. The resulte is
+    recursively until no further variables can be substituted. The result is
     returned either as string, or converted into a bool (True/False).
 
     Args:
@@ -20,22 +20,28 @@ def render_string(string, recursive=True, boolean=False, **kwargs):
             (i.e. the return value is a string). If true, the rendered
             string is evaluated in a boolean context in Jinja2.  The return
             value is true or false in this case.
-        kwargs: A dict, a dict subclass or some keyword arguments, which
-            provide the context for jinja2.Template().render()
+        context (dict): A dictionary that provides the context for
+            jinja2.Template().render()
 
     Returns:
-        str: The rendered string (of boolean==False), or
+        str: The rendered string (of boolean==False), OR
         bool: The rendered string, evaluated as bool (if boolean=True)
     """
-    rendered_string = Template(string).render(**kwargs)
+
+    def render_with_context(string_arg):
+        try:
+            return Template(string_arg).render(context)
+        except TemplateSyntaxError:
+            raise RuntimeError(f"Syntax error while rendering template string '{string}'"
+                               f"{' in boolean context' if boolean else ''}")
+
+    rendered_string = render_with_context(string)
     if recursive:
-        while True:
-            next_rendered = Template(rendered_string).render(**kwargs)
-            if rendered_string == next_rendered:
-                break
-            else:
-                rendered_string = next_rendered
+        next_rendered_string = render_with_context(rendered_string)
+        while rendered_string != next_rendered_string:
+            rendered_string = next_rendered_string
+            next_rendered_string = render_with_context(rendered_string)
     if boolean:
-        expr = '{% if '+rendered_string+' %}1{% else %}0{% endif %}'
-        return bool(strtobool(Template(expr).render(**kwargs)))
+        expr = f"{{% if {rendered_string} %}}1{{% else %}}0{{% endif %}}"
+        return bool(strtobool(render_with_context(expr)))
     return rendered_string
