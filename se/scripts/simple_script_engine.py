d6c202919d54d5c76e663e6d25a84a10175778fd
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-09-10 09:49:22 +0200

    Restructure Exit task
    Stop via ScriptEngineStopException and introduce logging in ScriptEngine

--- se/scripts/simple_script_engine.py
+++ se/scripts/simple_script_engine.py
@@ -4,14 +4,31 @@ SimpleScriptEngine is a simplistic script engine. It runs all jobs sequentially
 on the local machine, without consideration of job contexts.
 """
 
+import sys
+import logging
+
+from se.scripts.logging import app_logger
+from se.exceptions import ScriptEngineStopException
+
 class SimpleScriptEngine:
     """Simplistic script engine for ScriptEngine.
     """
+
+    def __init__(self, log_level=logging.INFO):
+        self._logger = app_logger(self.__class__.__name__, level=log_level)
+
+    def _guarded_run(self, item, context):
+        try:
+            item.run(context)
+        except ScriptEngineStopException as e:
+            self._logger.info(e)
+            sys.exit()
+
     def run(self, script, context):
         for script_item in script if isinstance(script, list) else [script]:
             try:
                 # Assume script_item is a task (i.e. has a run() method)
-                script_item.run(context)
+                self._guarded_run(script_item, context)
             except AttributeError: # script_item is a job, not a task
                 if script_item.when(context):
                     none_loop = object() # just make sure none_loop is unique
@@ -21,7 +38,7 @@ class SimpleScriptEngine:
                         for todo in script_item.todo:
                             try:
                                 # Same as above, assume task ...
-                                todo.run(context)
+                                self._guarded_run(todo, context)
                             except AttributeError:
                                 # ... otherwise, recurse for job
                                 self.run(todo, context)
--- se/tasks/exit.py
+++ se/tasks/exit.py
@@ -3,17 +3,18 @@
 import sys
 
 from se.tasks import Task
+from se.exceptions import ScriptEngineStopException
 
 
 class Exit(Task):
     """Exit task, stops by calling sys.exit
     """
-    def __init__(self, parameters):
-        super().__init__(__name__, parameters, required_parameters=["msg"])
+    def __init__(self, parameters=None):
+        super().__init__(__name__, parameters)
 
     def __str__(self):
-        return f"Exit: {self.msg}"
+        return f"Exit {getattr(self, 'msg', '')}"
 
     def run(self, context):
-        self.log_info(self.msg)
-        sys.exit()
+        self.log_info(getattr(self, "msg", "Requesting ScriptEngine to stop"))
+        raise ScriptEngineStopException("Exit task requests stopping ScriptEngine")
