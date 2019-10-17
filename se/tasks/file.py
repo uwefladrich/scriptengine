4a1d09c00a3c84de069d26e82973e927b37e59df
Uwe Fladrich <uwe.fladrich@smhi.se>
2019-10-17 08:57:47 +0200

    Replace term src/dst by path in Remove and MakeDir

--- se/tasks/file.py
+++ se/tasks/file.py
@@ -63,38 +63,38 @@ class Link(Task):
 
 class Remove(Task):
     """ScriptEngine Remove task: Removes (deletes) files or directories.
-    Directories are removed recursively. It needs the 'src' parameter when it
-    is created, providing the paths that is to be removed.
+    Directories are removed recursively. It needs the 'path' parameter when it
+    is created, providing the path to the file or directory to be removed.
     """
     def __init__(self, parameters):
-        super().__init__(__name__+".remove", parameters, required_parameters=["src"])
+        super().__init__(__name__+".remove", parameters, required_parameters=["path"])
 
     def run(self, context):
-        src_path = render_string(self.src, context)
-        if os.path.exists(src_path):
-            if os.path.isdir(src_path):
-                self.log_info(f"Removing directory '{src_path}'")
-                shutil.rmtree(src_path)
+        path = render_string(self.src, context)
+        if os.path.exists(path):
+            if os.path.isdir(path):
+                self.log_info(f"Removing directory '{path}'")
+                shutil.rmtree(path)
             else:
-                self.log_info(f"Removing file '{src_path}'")
-                os.remove(src_path)
+                self.log_info(f"Removing file '{path}'")
+                os.remove(path)
         else:
-            self.log_info(f"Non-existing path '{src_path}'; nothing removed")
+            self.log_info(f"Non-existing path '{path}'; nothing removed")
 
 
 class MakeDir(Task):
-    """ScriptEngine MakeDir task: Creates a directory. It needs the 'dst'
+    """ScriptEngine MakeDir task: Creates a directory. It needs the 'path'
     parameter when it is created, providing the path of the new directory
     """
     def __init__(self, parameters):
-        super().__init__(__name__+".make_dir", parameters, required_parameters=["dst"])
+        super().__init__(__name__+".make_dir", parameters, required_parameters=["path"])
 
     def run(self, context):
-        dst_path = render_string(self.dst, context)
-        if os.path.isdir(dst_path):
-            self.log_info(f"Directory '{dst_path}' exists already.")
-        elif os.path.isfile(dst_path):
-            raise RuntimeError(f"'{dst_path}' is a file, can't create directory")
+        path = render_string(self.dst, context)
+        if os.path.isdir(path):
+            self.log_info(f"Directory '{path}' exists already.")
+        elif os.path.isfile(path):
+            raise RuntimeError(f"'{path}' is a file, can't create directory")
         else:
-            self.log_info(f"Creating directory '{dst_path}'")
-            os.makedirs(dst_path)
+            self.log_info(f"Creating directory '{path}'")
+            os.makedirs(path)
