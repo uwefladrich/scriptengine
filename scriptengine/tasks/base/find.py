fa70bfd2e8ce8e4372893c5996dbfaf34abd7fbf
Uwe Fladrich <uwe.fladrich@smhi.se>
2020-03-27 09:14:47 +0100

    Implement 'depth' for find task (#10)
    Squashed commit of the following:
    
    commit 8aa7ddef11e825653f52c48f143501391f865a03
    Author: Uwe Fladrich <uwe.fladrich@smhi.se>
    Date:   Fri Mar 27 09:13:56 2020 +0100
    
        Normalise paths before appending to result
    
    commit a79ec05e55041059bc1868686d9e78d87bbaab40
    Author: Uwe Fladrich <uwe.fladrich@smhi.se>
    Date:   Fri Mar 27 09:12:09 2020 +0100
    
        Implement 'depth' for find task

--- scriptengine/tasks/base/find.py
+++ scriptengine/tasks/base/find.py
@@ -14,17 +14,26 @@ class Find(Task):
         super().__init__(__name__, parameters, required_parameters=["path"])
 
     def run(self, context):
-        path = j2render(self.path, context)
-        pattern = j2render(getattr(self, "pattern", "*"), context)
-        find_type = j2render(getattr(self, "type", "file"), context)
+        path = os.path.normpath(j2render(self.path, context))
+        path_depth = path.count(os.path.sep)
 
+        pattern = j2render(getattr(self, "pattern", "*"), context)
+
+        find_type = j2render(getattr(self, "type", "file"), context)
         if find_type not in ("file", "dir"):
-            self.log_error(f"Invalid type: {find_type} "
+            self.log_error(f"Invalid 'type': {find_type} "
                            "(must be either 'file' or 'dir')")
             raise ScriptEngineStopException(f"Invalid 'type' argument in find task")
 
+        max_depth = j2render(getattr(self, "depth", "-1"), context)
+        try:
+            max_depth = max(-1, int(max_depth))
+        except (ValueError, TypeError):
+            self.log_error(f"Invalid 'depth': {max_depth} (must be an integer)")
+            raise ScriptEngineStopException(f"Invalid 'depth' argument in find task")
+
         self.log_info(f"Find {find_type} with pattern '{pattern}' "
-                      f"in {path}")
+                      f"in {path} (with max depth={max_depth})")
         result = []
         for root, dirs, files in os.walk(path):
             if find_type == "file":
@@ -33,7 +42,9 @@ class Find(Task):
                 files_or_dirs = dirs
             for name in files_or_dirs:
                 if fnmatch.fnmatch(name, pattern):
-                    result.append(os.path.join(root, name))
+                    result.append(os.path.normpath(os.path.join(root, name)))
+            if max_depth >= 0 and root.count(os.path.sep) >= path_depth+max_depth:
+                del dirs[:]
         if result:
             self.log_debug(f"Found: {result}")
         else:
