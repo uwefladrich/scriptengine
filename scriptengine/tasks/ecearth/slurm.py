d2b26d230cb4f6fc3465c1ff17dfb32ad2d634f5
Uwe Fladrich <uwe.fladrich@smhi.se>
2020-04-16 16:23:12 +0200

    Implement submit_from_batch_job for sbatch task
    This option allows to chose whether the sbatch tasks submits a job even
    it is already running from within a SLURM batch job. The default is
    'no'.
    
    commit 1e365ed17646dd173a5d7826bc6c29f69ff95e76
    Author: Uwe Fladrich <uwe.fladrich@smhi.se>
    Date:   Thu Apr 16 15:35:22 2020 +0200
    
        Clarify code comments and debug message
    
    commit 71d1d149b214046f99028202f6f08743604b654c
    Author: Uwe Fladrich <uwe.fladrich@smhi.se>
    Date:   Thu Apr 16 09:36:54 2020 +0200
    
        Implement 'submit_from_batch_job' and add comments
    
    commit ee8b5a213e41a11a827418fc8d05ae36f6bf4cb5
    Author: Uwe Fladrich <uwe.fladrich@smhi.se>
    Date:   Thu Apr 16 09:04:10 2020 +0200
    
        Remove 'set_context' arg from sbatch task
    
        Which didn't work anyway.
    
    commit 62b3173ff730724d5e13faa4822a2ecf734684a5
    Author: Uwe Fladrich <uwe.fladrich@smhi.se>
    Date:   Thu Apr 16 09:03:04 2020 +0200
    
        Change name of 'se_scripts' argument to 'scripts'

--- scriptengine/tasks/ecearth/slurm.py
+++ scriptengine/tasks/ecearth/slurm.py
@@ -13,33 +13,49 @@ class Sbatch(Task):
     """
 
     def __init__(self, parameters):
-        super().__init__(__name__, parameters, required_parameters=["se_scripts"])
+        super().__init__(__name__, parameters, required_parameters=["scripts"])
 
     def run(self, context):
 
-        if os.environ.get("SLURM_JOB_NAME"):
-            context[getattr(self, "set_context", None) or "slurm"] = True
-            return
+        # Default is to _not_ submit a job if we are already within a batch job
+        submit_from_batch_job = False
 
+        # Get all arguments from the task description
+        # Almost all arguments are just passed to the 'sbatch' command, except
+        # for the 'scripts' and 'submit_from_batch_job', which are used here
         opt_args = []
         for opt, arg in self.__dict__.items():
-            if opt == "se_scripts":
+            if opt == "scripts":
                 if isinstance(arg, list):
                     scripts = [j2render(script, context) for script in arg]
                 else:
                     scripts = [j2render(arg, context)]
+                self.log_info(f"Submit job for {scripts}")
+            elif opt == "submit_from_batch_job":
+                submit_from_batch_job = arg
             elif not opt.startswith("_"):
                 opt_args.append(f"--{opt}")
                 if arg:
                     opt_args.append(j2render(arg, context))
 
+        # Check if we are within a SLURM batch job and if we want to submit a
+        # job from within a job
+        if os.environ.get("SLURM_JOB_NAME"):
+            if not submit_from_batch_job:
+                self.log_debug("Sbatch task from within a SLURM batch job "
+                               "and 'submit_from_batch_job' is not set or "
+                               "false: not submitting new job")
+                return
+
+        # Build the sbatch command line
         sbatch_cmd = ["sbatch"]
         sbatch_cmd.extend(map(str, opt_args))
         sbatch_cmd.append("se")
         sbatch_cmd.extend(map(str, scripts))
 
-        self.log_debug(f"{sbatch_cmd}")
+        self.log_debug(f"Sbatch command line: {sbatch_cmd}")
 
+        # Run sbatch command, with handling of errors
         try:
             subprocess.run(sbatch_cmd, check=True)
         except subprocess.CalledProcessError:
