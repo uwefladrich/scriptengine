1504672c52499849b82f1b81ae70374e92ddd7ae
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-10-25 09:46:21 +0200

    Add new tests for new base.context.from task

--- tests/tasks/base/test_context.py
+++ tests/tasks/base/test_context.py
@@ -51,3 +51,94 @@ def test_context_simple_set():
     ctx += ctx_upd
     assert ctx["foo"] == 1
     assert ctx["bar"] == 2
+
+
+def test_context_from_dict():
+    t = from_yaml(
+        """
+        base.context.from:
+            dict: {'foo': 1, 'bar': 2}
+        """
+    )
+    ctx = {}
+    ctx_upd = t.run(ctx)
+    ctx += ctx_upd
+    assert ctx["foo"] == 1
+    assert ctx["bar"] == 2
+
+
+def test_context_from_context_dict():
+    t1 = from_yaml(
+        """
+        base.context:
+            update:
+                foo: 1
+                bar: 2
+        """
+    )
+    t2 = from_yaml(
+        """
+        base.context.from:
+            dict: '{{update}}'
+        """
+    )
+    ctx = {}
+    ctx_upd = t1.run(ctx)
+    ctx += ctx_upd
+    ctx_upd = t2.run(ctx)
+    ctx += ctx_upd
+    assert ctx["foo"] == 1
+    assert ctx["bar"] == 2
+
+
+def test_context_update_from_context_dict():
+    t1 = from_yaml(
+        """
+        base.context:
+            update:
+                foo: 5
+        """
+    )
+    t2 = from_yaml(
+        """
+        base.context:
+            foo: 1
+            bar: 2
+        """
+    )
+    t3 = from_yaml(
+        """
+        base.context.from:
+            dict: '{{update}}'
+        """
+    )
+    ctx = {}
+    ctx_upd = t1.run(ctx)
+    ctx += ctx_upd
+    ctx_upd = t2.run(ctx)
+    ctx += ctx_upd
+    ctx_upd = t3.run(ctx)
+    ctx += ctx_upd
+    assert ctx["foo"] == 5
+    assert ctx["bar"] == 2
+
+
+def test_context_from_file(tmp_path):
+    f = tmp_path / "f.yml"
+    f.write_text(
+        """
+        foo: 1
+        bar: 2
+        """
+    )
+    t = from_yaml(
+        f"""
+        base.context.from:
+            file: {f}
+        """
+    )
+    ctx = {}
+    ctx_upd = t.run(ctx)
+    ctx += ctx_upd
+    assert ctx["foo"] == 1
+    assert ctx["bar"] == 2
