d372a2f08cf08c0700001acbf58aaa96a889df43
Uwe Fladrich <49554848+uwefladrich@users.noreply.github.com>
2022-10-29 21:33:37 +0200

    Fix #74: base.find correct update context  (#75)
    * Add test for base.find
    
    * Make base.find return ContextUpdate
    
    * isort+black

---
+++ tests/tasks/base/test_find.py
@@ -0,0 +1,127 @@
+import yaml
+
+from scriptengine.yaml.parser import parse
+
+
+def from_yaml(string):
+    return parse(yaml.load(string, Loader=yaml.FullLoader))
+
+
+def test_find_basic(tmp_path):
+
+    subdir = tmp_path / "foo"
+    subdir.mkdir()
+
+    (tmp_path / "bar-0.txt").touch()
+    (subdir / "bar-1.txt").touch()
+    (subdir / "bar-2.txt").touch()
+
+    t = from_yaml(
+        f"""
+        base.find:
+            path: {tmp_path}
+            pattern: 'bar-?.txt'
+        """
+    )
+    ctx = {}
+    ctx += t.run(ctx)
+    assert set(ctx["result"]) == {
+        f"{tmp_path}/bar-0.txt",
+        f"{subdir}/bar-1.txt",
+        f"{subdir}/bar-2.txt",
+    }
+
+    t = from_yaml(
+        f"""
+        base.find:
+            path: {subdir}
+            pattern: 'bar-?.txt'
+        """
+    )
+    ctx = {}
+    ctx += t.run(ctx)
+    assert set(ctx["result"]) == {
+        f"{subdir}/bar-1.txt",
+        f"{subdir}/bar-2.txt",
+    }
+
+
+def test_find_with_set(tmp_path):
+
+    subdir = tmp_path / "foo"
+    subdir.mkdir()
+
+    (subdir / "bar-1.txt").touch()
+    (subdir / "bar-2.txt").touch()
+
+    t = from_yaml(
+        f"""
+        base.find:
+            path: {subdir}
+            pattern: 'bar-?.txt'
+            set: foobar
+        """
+    )
+    ctx = {}
+    ctx += t.run(ctx)
+    assert set(ctx["foobar"]) == {
+        f"{subdir}/bar-1.txt",
+        f"{subdir}/bar-2.txt",
+    }
+
+
+def test_find_with_depth(tmp_path):
+
+    subdir = tmp_path / "foo" / "foo_2"
+    subdir.mkdir(parents=True)
+
+    (subdir / "bar-1.txt").touch()
+    (subdir / "bar-2.txt").touch()
+
+    t = from_yaml(
+        f"""
+        base.find:
+            path: {tmp_path}
+            pattern: 'bar-?.txt'
+            depth: 1
+        """
+    )
+    ctx = {}
+    ctx += t.run(ctx)
+    assert ctx["result"] == []
+
+
+def test_find_with_file_type(tmp_path):
+
+    subdir = tmp_path / "foo"
+    subdir.mkdir()
+
+    (subdir / "bar-1.txt").touch()
+    (subdir / "bar-2.txt").mkdir()
+
+    t = from_yaml(
+        f"""
+        base.find:
+            path: {subdir}
+            pattern: 'bar-?.txt'
+        """
+    )
+    ctx = {}
+    ctx += t.run(ctx)
+    assert set(ctx["result"]) == {
+        f"{subdir}/bar-1.txt",
+    }
+
+    t = from_yaml(
+        f"""
+        base.find:
+            path: {subdir}
+            pattern: 'bar-?.txt'
+            type: dir
+        """
+    )
+    ctx = {}
+    ctx += t.run(ctx)
+    assert set(ctx["result"]) == {
+        f"{subdir}/bar-2.txt",
+    }
