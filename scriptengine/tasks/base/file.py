96ace3388c424e63909b9134020622157826027e
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-11-01 16:46:59 +0100

    Restructure base.link

--- scriptengine/tasks/base/file.py
+++ scriptengine/tasks/base/file.py
@@ -17,28 +17,3 @@ def _getarg_path(self, name, context={}, **kwargs):
     except TypeError:
         self.log_error(f'Invalid "{name}" argument, "{arg}" is not a path')
         raise ScriptEngineTaskRunError
-
-
-class Link(Task):
-    """ScriptEngine Link task: Creates symlinks. It needs 'src' and 'dst'
-    parameters when it is created, providing the source and destination paths
-    for the link.
-    """
-
-    _required_arguments = (
-        "src",
-        "dst",
-    )
-
-    def __init__(self, arguments):
-        Link.check_arguments(arguments)
-        super().__init__(arguments)
-
-    @timed_runner
-    def run(self, context):
-        src = _getarg_path(self, "src", context)
-        dst = _getarg_path(self, "dst", context)
-        self.log_info(f"Create link: {src} --> {dst}")
-        dst.symlink_to(src)
-        if not dst.exists():
-            self.log_warning(f"Created dangling symlink: {dst}-->{src}")
