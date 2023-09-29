ea877eb44c0ad84ba0282ada82764ff90915c100
Uwe Fladrich <uwe.fladrich@smhi.se>
2023-09-29 17:27:22 +0200

    Add further tests for Context

--- tests/context/test_context_dotted_keys.py
+++ tests/context/test_context_dotted_keys.py
@@ -1,3 +1,5 @@
+import pytest
+
 from scriptengine.context import Context
 
 
@@ -18,6 +20,29 @@ def test_set_dotted_key():
     assert c["foo"]["bar"] == 1
 
 
+def test_set_int_key():
+    c = Context()
+    c[1] = "foo"
+    assert c[1] == "foo"
+
+
 def test_mixed_access():
     c = Context({"foo": {"bar": {"baz": 1}}})
     assert c["foo.bar"]["baz"] == 1
+
+
+def test_too_many_dots():
+    c = Context({"foo": {"bar": 1}})
+    with pytest.raises(KeyError):
+        c["foo.bar.baz"]
+
+
+def test_wrong_key_type():
+    c = Context({"foo": {"bar": 1}})
+    with pytest.raises(KeyError):
+        c[1]
+
+
+def test_add_wrong_type():
+    with pytest.raises(TypeError):
+        Context() + 1
