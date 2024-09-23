066ab34b0aab758c83b2d3893809ef21aecf8a82
Uwe Fladrich <uwe.fladrich@smhi.se>
2024-09-23 20:42:05 +0200

    Add tests for task timing in jobs

--- tests/tasks/base/test_task_timing.py
+++ tests/tasks/base/test_task_timing.py
@@ -1,11 +1,18 @@
 from time import sleep
 
 import pytest
+import yaml
 
 from scriptengine.context import Context
 from scriptengine.exceptions import ScriptEngineTaskArgumentMissingError
+from scriptengine.tasks.base.echo import Echo
 from scriptengine.tasks.base.task_timer import TaskTimer
 from scriptengine.tasks.core import Task, timed_runner
+from scriptengine.yaml.parser import parse
+
+
+def from_yaml(string):
+    return parse(yaml.load(string, Loader=yaml.FullLoader))
 
 
 class WaitASecond(Task):
@@ -51,10 +58,54 @@ def test_task_timing():
     )
 
     context += timer.run(context)
-    timed.run(context)
+    context += timed.run(context)
 
     assert context["se.tasks.timing"]["mode"] == timer_mode
     assert context["se.tasks.timing"]["logging"] == timer_logging
 
     assert context["se.tasks.timing.timers.classes"][timed.__class__.__name__] > 1.0
     assert context["se.tasks.timing.timers.instances"][timed.id] > 1.0
+
+
+def test_task_timer_after_echo():
+
+    context = Context(
+        {
+            "se.tasks.timing": {
+                "mode": False,
+                "logging": None,
+                "timers": {},
+            }
+        }
+    )
+    context += TaskTimer({"mode": "classes"}).run(context)
+    context += Echo({"msg": "Hello!"}).run(context)
+
+    assert "se.tasks.timing.timers.classes" in context
+    assert context["se.tasks.timing.timers.classes.Echo"] > 0.0
+
+
+def test_task_timer_in_job():
+
+    context = Context(
+        {
+            "se.tasks.timing": {
+                "mode": False,
+                "logging": None,
+                "timers": {},
+            }
+        }
+    )
+    context += TaskTimer({"mode": "classes"}).run(context)
+
+    job = from_yaml(
+        """
+        do:
+            - base.echo:
+                msg: Hello
+        """
+    )
+    context += job.run(context)
+
+    assert "se.tasks.timing.timers.classes" in context
+    assert context["se.tasks.timing.timers.classes.Echo"] > 0.0
