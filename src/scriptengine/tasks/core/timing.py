d5712739f54c869d171e4c503e9fd0b5f926ac4f
Uwe Fladrich <uwe.fladrich@smhi.se>
2024-09-23 18:40:14 +0200

    isort+black and use dotted keys in timing.py

--- src/scriptengine/tasks/core/timing.py
+++ src/scriptengine/tasks/core/timing.py
@@ -1,30 +1,30 @@
 """ScriptEngine timing for tasks: provides timing of Task classes .run() method
 """
 
-import time
 import functools
+import time
 
 
 def timed_runner(func):
     """Wrapper for run() functions of SE tasks. To be used as decorator as follows:
 
-        from scriptengine.task_timing import timed_runner
-        class Foo(Task):
+    from scriptengine.task_timing import timed_runner
+    class Foo(Task):
+        # ...
+        @timed_runner
+        def run(self, context):
             # ...
-            @timed_runner
-            def run(self, context):
-                # ...
     """
 
     @functools.wraps(func)
     def wrap_timed(self, context):
 
         try:
-            mode = context['se']['tasks']['timing']['mode']
+            mode = context["se.tasks.timing.mode"]
         except KeyError:
             mode = False
 
-        if mode in ('basic', 'classes', 'instances'):
+        if mode in ("basic", "classes", "instances"):
 
             # timed function call
             start_tic = time.perf_counter()
@@ -32,21 +32,21 @@ def timed_runner(func):
             elapsed_time = time.perf_counter() - start_tic
 
             # logging
-            if context['se']['tasks']['timing']['logging'] == 'info':
-                self.log_info(f'Elapsed time: {elapsed_time:0.4f} seconds')
-            elif context['se']['tasks']['timing']['logging'] == 'debug':
-                self.log_debug(f'Elapsed time: {elapsed_time:0.4f} seconds')
+            if context["se.tasks.timing.logging"] == "info":
+                self.log_info(f"Elapsed time: {elapsed_time:0.4f} seconds")
+            elif context["se.tasks.timing.logging"] == "debug":
+                self.log_debug(f"Elapsed time: {elapsed_time:0.4f} seconds")
 
             # update timers
-            if mode in ('classes', 'instances'):
-                timers = context['se']['tasks']['timing']['timers']
+            if mode in ("classes", "instances"):
+                timers = context["se.tasks.timing.timers"]
 
-                class_t = timers.setdefault('classes', {})
+                class_t = timers.setdefault("classes", {})
                 class_t.setdefault(self.__class__.__name__, 0)
                 class_t[self.__class__.__name__] += elapsed_time
 
-                if mode == 'instances':
-                    instance_t = timers.setdefault('instances', {})
+                if mode == "instances":
+                    instance_t = timers.setdefault("instances", {})
                     instance_t.setdefault(self.id, 0)
                     instance_t[self.id] += elapsed_time
 
