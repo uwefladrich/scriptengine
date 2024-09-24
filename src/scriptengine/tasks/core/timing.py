0e395f457d9f635765fd05ecf9971fbd45f60aed
Uwe Fladrich <49554848+uwefladrich@users.noreply.github.com>
2024-09-24 08:13:54 +0200

    Fix: task timing in jobs does not work (#115)
    * Add tests for task timing in jobs
    
    * isort+black and use dotted keys in timing.py
    
    * Use dict.get() instead of checking for KeyError
    
    * Make sure timed_runner returns context update
    
    * Update CHANGES

--- src/scriptengine/tasks/core/timing.py
+++ src/scriptengine/tasks/core/timing.py
@@ -1,56 +1,61 @@
 """ScriptEngine timing for tasks: provides timing of Task classes .run() method
 """
 
-import time
+import copy
 import functools
+import time
+
+from scriptengine.context import Context
 
 
 def timed_runner(func):
     """Wrapper for run() functions of SE tasks. To be used as decorator as follows:
 
-        from scriptengine.task_timing import timed_runner
-        class Foo(Task):
+    from scriptengine.task_timing import timed_runner
+    class Foo(Task):
+        # ...
+        @timed_runner
+        def run(self, context):
             # ...
-            @timed_runner
-            def run(self, context):
-                # ...
     """
 
     @functools.wraps(func)
     def wrap_timed(self, context):
 
-        try:
-            mode = context['se']['tasks']['timing']['mode']
-        except KeyError:
-            mode = False
+        mode = context.get("se.tasks.timing.mode")
 
-        if mode in ('basic', 'classes', 'instances'):
+        if mode in ("basic", "classes", "instances"):
 
             # timed function call
             start_tic = time.perf_counter()
-            func_return_value = func(self, context)
+            context_update = func(self, context) or Context()
             elapsed_time = time.perf_counter() - start_tic
 
             # logging
-            if context['se']['tasks']['timing']['logging'] == 'info':
-                self.log_info(f'Elapsed time: {elapsed_time:0.4f} seconds')
-            elif context['se']['tasks']['timing']['logging'] == 'debug':
-                self.log_debug(f'Elapsed time: {elapsed_time:0.4f} seconds')
+            if context["se.tasks.timing.logging"] == "info":
+                self.log_info(f"Elapsed time: {elapsed_time:0.4f} seconds")
+            elif context["se.tasks.timing.logging"] == "debug":
+                self.log_debug(f"Elapsed time: {elapsed_time:0.4f} seconds")
 
             # update timers
-            if mode in ('classes', 'instances'):
-                timers = context['se']['tasks']['timing']['timers']
+            if mode in ("classes", "instances"):
+                timers = copy.deepcopy(context["se.tasks.timing.timers"])
 
-                class_t = timers.setdefault('classes', {})
+                class_t = timers.setdefault("classes", {})
                 class_t.setdefault(self.__class__.__name__, 0)
                 class_t[self.__class__.__name__] += elapsed_time
 
-                if mode == 'instances':
-                    instance_t = timers.setdefault('instances', {})
+                if mode == "instances":
+                    instance_t = timers.setdefault("instances", {})
                     instance_t.setdefault(self.id, 0)
                     instance_t[self.id] += elapsed_time
 
-            return func_return_value
+                timer_update = Context()
+                timer_update["se.tasks.timing.timers"] = timers
+
+                context_update += timer_update
+
+            return context_update
 
         # if no timing, just return the function
         return func(self, context)
